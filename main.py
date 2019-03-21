ba888eef39618dd16ce500aa4512444b35597aea
Aryaz Eghbali <aryaz.egh@gmail.com>
2019-03-21 16:08:29 +0100

    Added Model 2

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from dateutil import parser as parser
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
@@ -77,11 +76,40 @@ def market_share_1(mining_hw, btc_hashrate):
 	market_share = pd.DataFrame(mrkt_shr_hist, columns=cols)
 	return market_share
 
+def market_share_2(mining_hw, btc_hashrate):
+	# Model 2: Assuming the 3 newest hardware are added in increasing hash rates and the oldest gets removed in the decreases. Maybe some people turn off their HW but then decide to use them again
+	cols = ['date'] + mining_hw['device'].values.tolist()
+	
+	mrkt_shr_hist = []
+	curr_mrkt_shr = [0.0 for i in range(len(cols)-1)]
+	for row in btc_hashrate.itertuples():
+		if row.change >= 0:
+			n = 3 # Get the newest n HW
+			newest = mining_hw[mining_hw['release_date'] < row.date].tail(n).index.tolist()
+			for i in range(len(newest)-1):
+				curr_mrkt_shr[newest[(-i-1)]] += row.change * (1.0/(2.0**(i+1)))
+			curr_mrkt_shr[newest[0]] += row.change * (1.0/(2.0**(len(newest)-1)))
+		else:
+			old_ones = mining_hw[mining_hw['release_date'] < row.date].index.tolist()
+			leftover = row.change
+			i = 0
+			while leftover < 0:
+				if curr_mrkt_shr[i] + leftover >= 0:
+					curr_mrkt_shr[i] += leftover
+					leftover = 0
+				else:
+					leftover += curr_mrkt_shr[i]
+					curr_mrkt_shr[i] = 0
+					i += 1
+		mrkt_shr_hist.append([row.date] + curr_mrkt_shr)
+	market_share = pd.DataFrame(mrkt_shr_hist, columns=cols)
+	return market_share
+
 def main():
 	
 	mining_hw, btc_price, btc_hashrate, btc_tx_fees, btc_txs = prepare_data()
 	
-	market_share = market_share_1(mining_hw, btc_hashrate)
+	market_share = market_share_2(mining_hw, btc_hashrate)
 	
 	# Revenue in USD per day
 	btc_rev = pd.DataFrame([], columns=['date', 'revenue'])
@@ -89,7 +117,7 @@ def main():
 	btc_rev['revenue'] = btc_price['price']*(24*6*btc_price['block_reward'] + btc_tx_fees['fee']*btc_txs['n'])
 
 	# Cost in USD per day
-	electricity_price = 0.08 # Price in USD/KWh
+	electricity_price = 0.04 # Price in USD/KWh
 	btc_cost = pd.DataFrame([], columns=['date', 'cost'])
 	btc_cost['date'] = market_share['date']
 	btc_cost['cost'] = (market_share.loc[:, market_share.columns != 'date'].dot(mining_hw['power_usage'].values))*3600*24*electricity_price
@@ -97,7 +125,7 @@ def main():
 	cost_share = (market_share.loc[:, market_share.columns != 'date']*mining_hw['power_usage'].values)*3600*24*electricity_price
 
 	fig, ax = plt.subplots()
-	ax.set_yscale('log')
+	ax.set_yscale('linear')
 	y = []
 	for i in cost_share.columns.values:
 		y.append(cost_share[i])
